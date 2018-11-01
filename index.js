04f2a3e77e89ab357bda39fa64488538aaae9ff5
Wasif Khan <waasif.khaan@gmail.com>
2018-11-01 01:37:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ const newID = 1000000000 + oldID;
 
 let ageIsValid = Number.isInteger(currentAge);
 let randomNumber = Math.floor(Math.random() * 20) + 1;
-let randomInteger= Math.random(Number.parseInt());
+let randomInteger= Number.parseInt(Math.random());
